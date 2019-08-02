a39ada5364ec9ec01e28e53fae5f03b1573867f6
Layza Floriano <layzafloriano@gmail.com>
2019-08-02 10:24:27 -0300

    add login

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import './App.css';
 import { Switch, Route } from 'react-router-dom';
 import Signup from './components/auth/Signup';
 import Login from './components/auth/Login';
+import NewAdd from './components/ad/NewAdd';
+import ProtectedRoute from './components/auth/protected-route';
 
 function App() {
   return (
@@ -11,6 +13,7 @@ function App() {
           {/* <Route exact path="/" component={HomePage}/> */}
           <Route exact path="/signup" component={Signup}/>
           <Route exact path="/login" component={Login}/>
+          <Route exact path="/new-add" component={NewAdd}/>
       </Switch>
     </div>
   );
