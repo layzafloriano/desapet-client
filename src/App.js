58926ee93c205969b91f8bcf48fe41b41b8d2ff4
Layza Floriano <layzafloriano@gmail.com>
2019-08-04 21:42:24 -0300

    add home

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import './App.css';
 import { Switch, Route } from 'react-router-dom';
+import Home from './components/home/Home';
 import Signup from './components/auth/Signup';
 import Login from './components/auth/Login';
 import NewAdd from './components/ad/NewAdd';
@@ -45,7 +46,7 @@ class App extends Component {
         <div className="App">
           <Navbar userInSession={this.state.loggedInUser} getUser={this.getTheUser} />
           <Switch>
-            <Route exact path="/novo-anuncio" component={NewAdd}/>
+            <ProtectedRoute user={this.state.loggedInUser} path='/' component={Home} />
             <ProtectedRoute user={this.state.loggedInUser} path='/novo-anuncio' component={NewAdd} />
           </Switch>
         </div>
@@ -55,6 +56,7 @@ class App extends Component {
         <div className="App">
           <Navbar userInSession={this.state.loggedInUser} />
           <Switch>
+            <Route exact path='/' render={() => <Home getUser={this.getTheUser}/>}/>
             <Route exact path='/signup' render={() => <Signup getUser={this.getTheUser}/>}/>
             <Route exact path='/login' render={() => <Login getUser={this.getTheUser}/>}/>
           </Switch>
@@ -62,22 +64,6 @@ class App extends Component {
       );
     }
   }
-
-  // render() {
-  //   return (
-  //     <div className="App">
-  //         <Navbar userInSession={this.state.loggedInUser}/>
-  //         <Switch>
-  //           {/* <Route exact path="/" component={HomePage}/> */}
-  //           {/* <Route exact path="/signup" component={Signup}/> */}
-  //           {/* <Route exact path="/login" component={Login}/> */}
-  //           <Route exact path='/signup' render={() => <Signup getUser={this.getTheUser}/>}/>
-  //           <Route exact path='/login' render={() => <Login getUser={this.getTheUser}/>}/>
-  //           <Route exact path="/novo-anuncio" component={NewAdd}/>
-  //       </Switch>
-  //     </div>
-  //   );
-  // }
 }
 
 export default App;
